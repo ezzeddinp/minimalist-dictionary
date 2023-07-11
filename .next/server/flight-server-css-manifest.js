fe21bd8c551a07be8704c91a430824dec77f4c5b
ezzeddin <87559497+ezzeddinp@users.noreply.github.com>
2023-07-11 15:46:29 +0000

    11 Juli 2023

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/workspaces/minimalist-dictionary/src/app/layout.tsx": [
+      "/workspaces/minimalist-dictionary/src/styles/globals.css",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Inter\"}",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Lora\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Lora\"}",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Inconsolata\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Inconsolata\"}"
+    ]
+  },
+  "cssModules": {
+    "/workspaces/minimalist-dictionary/src/app/page": [
+      "/workspaces/minimalist-dictionary/src/styles/globals.css",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Inter\"}",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Lora\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Lora\"}",
+      "/workspaces/minimalist-dictionary/node_modules/next/font/google/target.css?{\"path\":\"src/contexts/FontContext.tsx\",\"import\":\"Inconsolata\",\"arguments\":[{\"subsets\":[\"latin\"],\"display\":\"swap\"}],\"variableName\":\"Inconsolata\"}"
+    ]
+  }
+}
